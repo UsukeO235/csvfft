e38d4045f617a5fa7c9e687e115343875b3d515c
UsukeO235 <usukeo235@gmail.com>
2021-09-26 02:48:37 +0900

    Add sample

---
+++ sample/generate.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import numpy as np
+
+FREQ = 100
+SAMPLE_FREQ = 1000
+N_SAMPLES = 20000
+
+omega = 2.0 * np.pi * FREQ
+
+ts = [1.0/float(SAMPLE_FREQ)*float(i) for i in range(N_SAMPLES)]
+xs = [np.sin(omega*t) + 0.1*np.sin(2.0*omega*t) for t in ts]
+
+with open( 'input.csv', 'w' ) as f:
+    f.write( 't\tx\n' )
+    for t, x in zip(ts, xs):
+        f.write( str(t) + '\t' + str(x) + '\n' )
